bd15cdbc29683b75acdf0cc739648d101afe8402
AK6263 <abhaykshirsagar01@gmail.com>
2022-03-10 20:28:01 +0530

    add: Designing the Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/screens/tasks_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
